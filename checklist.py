027e2bfb40797e898c443354bfb91ac8d87d31a1
Luke Parker <luke@Lukes-MacBook-Pro.local>
2019-09-02 15:53:30 -0700

    added mark_completed

--- checklist.py
+++ checklist.py
@@ -1,11 +1,5 @@
 checklist = list()
-checklist.append('Blue')
-print(checklist)
-checklist.append('Orange')
-print(checklist)
 
-def create(item):
-    checklist.append('Blue')
 
 #CREATE
 def create(item):
@@ -23,17 +17,30 @@ def update(index, item):
 def destroy(index):
     checklist.pop(index)
 
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+      print("{} {}".format(index, list_item))
+      index +=1
+
+def mark_completed(index):
+    update(index,'{} {}'.format('√',read(index)))
+    print(read(index))
 #TEST
 def test():
-  create("purple sox")
-  create("red cloak")
-  
-  print(read(0))
-  print(read(1))
-  
-  update(0, "purple socks")
-  destroy(1)
-  
-  print(read(0))
-  print(read(1))
+    create("purple sox")
+    create("red cloak")
+    
+    print(read(0))
+    print(read(1))
+    
+    update(0, "purple socks")
+    destroy(1)
+    
+    print(read(0))
+
+    list_all_items()
+
+    mark_completed(0)
 test()
+
